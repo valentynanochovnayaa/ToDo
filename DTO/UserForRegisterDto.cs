84b09e8a8d11594137e30284e38b6a1fe7af1b86
unknown <mimi.cry4848@gmail.com>
2020-11-03 18:24:41 +0200

    Add UserForLogin and UserForRegister DTO

---
+++ DTO/UserForRegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo.DTO
+{
+    public class UserForRegisterDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
