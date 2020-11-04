2792c0b139515a9dd3392a41a96e7b3d8cc89d4d
Valentyna <1005nochovnaya@gmail.com>
2020-11-04 19:01:28 +0200

    Add ServiceContainer

---
+++ Data/ServicesContainer.cs
@@ -0,0 +1,22 @@
+﻿using Data.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Services;
+using ToDo.Data;
+
+namespace Data
+{
+    public static class ServicesContainer
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {    
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options => {
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+
+        }
+    }
+}
