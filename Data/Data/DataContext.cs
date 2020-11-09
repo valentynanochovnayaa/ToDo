b6f316788b6913ecbf5612dac4711461461fcec1
unknown <mimi.cry4848@gmail.com>
2020-11-09 09:55:40 +0200

    Create migration

--- Data/Data/DataContext.cs
+++ Data/Data/DataContext.cs
@@ -1,24 +1,30 @@
-﻿using Domain.Entities;
+﻿using System;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data.Data
 {
-    public class DataContext: DbContext
+    public class DataContext: IdentityDbContext<User, Role, Guid>
     {
         public DataContext(DbContextOptions options):base(options)
         {}
+        public DataContext()
+        {
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
             builder.UseNpgsql("Host=localhost; Port=5432; Database=tododb; Username=postgres; password=mimicry");
         }
-        public DbSet<User> Users { get; set; }
+        public override DbSet<User> Users { get; set; }
         public DbSet<ToDoItem> ToDoItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<ToDoItem>()
                 .HasOne(u => u.User)
-                .WithMany(t => t.Tasks)
+                .WithMany(t => t.ToDoItems)
                 .HasForeignKey(t => t.UserId);
         }
         
